e5be3c113096806f418a2fc23274eba3d9e6471d
richard14092006 <richard1@unochapeco.edu.br>
2025-12-02 18:58:58 -0300

    Refactor Astronauta class for improved readability
    Implementado a validação de dados para que o Astronauta não tenha menos que 21 anos;

--- src/main/java/com/lunarsystems/model/Astronauta.java
+++ src/main/java/com/lunarsystems/model/Astronauta.java
@@ -1,42 +1,47 @@
 package com.lunarsystems.model;
 
-
 import java.io.Serializable;
-
 import org.dizitart.no2.objects.Id;
-
 
 public class Astronauta implements Serializable {
     @Id
-private String nome;
-private int idade;
-private String especialidade;
-private int horasVoo;
+    private String nome;
+    private int idade;
+    private String especialidade;
+    private int horasVoo;
 
-public Astronauta() {
-}   
+    public Astronauta() {
+    }
 
+    public Astronauta(String nome, int idade, String especialidade, int horasVoo) {
+        this.nome = nome;
 
-public Astronauta(String nome, int idade, String especialidade, int horasVoo) {
-this.nome = nome;
-this.idade = idade;
-this.especialidade = especialidade;
-this.horasVoo = horasVoo;
-}
+        setIdade(idade);
+        this.especialidade = especialidade;
+        this.horasVoo = horasVoo;
+    }
 
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
 
-public String getNome() { return nome; }
-public void setNome(String nome) { this.nome = nome; }
-public int getIdade() { return idade; }
-public void setIdade(int idade) { this.idade = idade; }
-public String getEspecialidade() { return especialidade; }
-public void setEspecialidade(String especialidade) { this.especialidade = especialidade; }
-public int getHorasVoo() { return horasVoo; }
-public void setHorasVoo(int horasVoo) { this.horasVoo = horasVoo; }
+    public int getIdade() { return idade; }
 
 
-@Override
-public String toString() {
-return nome + " (" + idade + " anos, " + especialidade + ", " + horasVoo + "h)";
-}
+    public void setIdade(int idade) {
+        if (idade < 21) {
+            throw new IllegalArgumentException("A idade mínima para um astronauta é 21 anos.");
+        }
+        this.idade = idade;
+    }
+
+
+    public String getEspecialidade() { return especialidade; }
+    public void setEspecialidade(String especialidade) { this.especialidade = especialidade; }
+    public int getHorasVoo() { return horasVoo; }
+    public void setHorasVoo(int horasVoo) { this.horasVoo = horasVoo; }
+
+    @Override
+    public String toString() {
+        return nome + " (" + idade + " anos, \n" + especialidade + ", \n" + horasVoo + "h)\n";
+    }
 }
